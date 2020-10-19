564d0f965f416363f86a261a0a5f44eec3e360ed
Eric Vallsmadella Crespo <eric.vallsmadella@students.salle.url.edu>
2020-10-19 23:32:30 +0200

    Feat: The direct clock class was created. The direct clock is a class that allows the Lamport algorithm to be deterministic. This class has the control of the timestamps of the system and not the ones of the requests of the system.

---
+++ src/main/java/DirectClock.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class DirectClock {
+    private ArrayList<Integer> clock;
+    private int myId;
+
+    public DirectClock(int myId, int numConnectedNodes) {
+        this.clock = new ArrayList<Integer>(numConnectedNodes);
+        this.myId = myId;
+    }
+
+    public void addNewNode(int nodeId){
+        //adding the nodeValue to the new node of the array
+        clock.add(nodeId,LamportNode.INFINITY);
+    }
+
+    public ArrayList<Integer> getClock() {
+        return clock;
+    }
+
+    public int deleteNode(int nodeId){
+        //if a node is deleted, we have to decrement our id by one
+        if(myId!=0 && myId>nodeId) myId--;
+        clock.remove(nodeId);
+        return myId;
+    }
+    public int getValue(int index){
+        return clock.get(index);
+    }
+    public void tick(){
+        this.clock.set(myId, this.clock.get(myId)+ 1);
+    }
+    public int requestAction(){
+        this.tick();
+        return this.clock.get(myId);
+    }
+    public void receiveAction(int sender, int sentValue){
+        this.clock.set(sender, Integer.max(sentValue, this.clock.get(myId)) );
+        this.clock.set(myId, Integer.max(sentValue, this.clock.get(myId)) + 1 );
+    }
+}
