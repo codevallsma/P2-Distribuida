e6bfd5ccaaafe924379525c4636a2f6595b5a087
Jcarri14 <j.carrion@students.salle.url.edu>
2020-12-17 09:29:03 +0100

    FEAT: commit left to do, in previous did not add this last file.

---
+++ src/main/java/MainSingleHeavyWeight.java
@@ -0,0 +1,28 @@
+import DataParser.Data;
+import DataParser.HeavyWeight;
+import DataParser.Parser;
+import Processes.HeavyWeight.HeavyweightProcess;
+import Utils.Launch;
+import Utils.Utils;
+
+import java.io.IOException;
+
+public class MainSingleHeavyWeight {
+    public static void main(String args[]) {
+        String hwType = args[0];
+        Data nodeNetwork = Parser.parseJson("NetworkConfigLamport.json");
+        HeavyWeight hw = nodeNetwork.getHeavyWeights().stream().filter(e -> e.getType().compareTo(hwType) == 0).findFirst().get();
+        HeavyWeight connectedTo = nodeNetwork.getHeavyWeights().stream().filter(e -> e.getType().compareTo(hwType) != 0).findFirst().get();
+        if (hw != null && connectedTo != null) {
+            HeavyweightProcess process = new HeavyweightProcess(hw, connectedTo, hw.getConnectToOther());
+            process.initBaseConnections();
+            while (!process.isReady()) {
+                System.out.println("(" + hw.getName() + ") Waiting to be ready...");
+                Utils.timeWait(5000);
+            }
+            process.doSomething();
+        } else {
+            System.err.println("Error en els paràmetres d'entrada!");
+        }
+    }
+}
